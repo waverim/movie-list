172c8388d0975ce7b88e0b8c46d3b918668996cc
Waverim <waverimax@gmail.com>
2015-01-20 20:12:27 +0800

    fix some little problems

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 ## Run
 
-    python src/server.py
+    $ python src/server.py
       
 
 ## Data Sources
--- src/server.py
+++ src/server.py
@@ -7,24 +7,7 @@ import upcoming
 
 class Index(tornado.web.RequestHandler):
     def get(self):
-        arr = nowplaying.get_nowplaying()
-
-        self.write('<h2>Now Playing</h2>')
-        for i in arr:
-            data = json.loads(i)
-            self.write('<p><a href="http://movie.douban.com/subject/' + data["id"] + '/">')
-            self.write(' ' + data["title"] + ' </a></p>')
-            self.write('<p style="text-indent: 2em;">Score: ' + data["score"]+ '</p>')
-            self.write('<p style="text-indent: 2em;">Actors: ' + data["actors"]+ '</p>')
-
-        up = upcoming.get_upcoming()
-
-        self.write('<h2>Upcoming</h2>')
-        for i in up:
-            data = json.loads(i)
-            self.write('<p><a href="http://movie.douban.com/subject/' + data["id"] + '/">')
-            self.write(' ' + data["title"] + ' </a></p>')
-            self.write('<p style="text-indent: 2em;">Coming date: ' + data["date"]+ '</p>')
+        self.write('<a href="/nowplaying">Nowplaying</a><br><a href="/upcoming">Upcoming</a>')
 
 
 class Nowplaying(tornado.web.RequestHandler):
